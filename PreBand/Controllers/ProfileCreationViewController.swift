3db1f26d6cc0e284c683a6d6bbcd87397ebc7131
Karthik.Singh <Karthik.Singh73@Gmail.com>
2019-03-25 21:45:30 -0500

    adding second view

---
+++ PreBand/Controllers/ProfileCreationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileCreationViewController.swift
+//  PreBand
+//
+//  Created by Karthik Singh on 3/25/19.
+//
+
+import UIKit
+
+class ProfileCreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
